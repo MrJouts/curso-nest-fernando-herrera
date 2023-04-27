6448828848092bc5d80b342186895a138f94a0c9
Emiliano Hotes <emiliano.hotes@despegar.com>
2023-04-27 12:00:49 -0300

    feat: added getter

--- 01-typescript-intro/src/bases/03-classes.ts
+++ 01-typescript-intro/src/bases/03-classes.ts
@@ -12,9 +12,15 @@
 
 // Definir clase de forma corta
 export class Pokemon {
+    get imageUrl(): string {
+        return `https://pokemon.com/${this.id}.jpg`;
+    }
+
     constructor(public readonly id: number, public name: string) {
         console.log("constructor llamado");
     }
 }
 
 export const charmander = new Pokemon(4, "Charmander");
+
+console.log(charmander);
