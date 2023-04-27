b4e158e34300ab628c550c80584c8336d8a9422e
Emiliano Hotes <emiliano.hotes@despegar.com>
2023-04-27 12:27:41 -0300

    feat: added types to http response

--- 01-typescript-intro/src/bases/03-classes.ts
+++ 01-typescript-intro/src/bases/03-classes.ts
@@ -1,4 +1,8 @@
 import axios from "axios";
+import {
+    Move,
+    PokeapiResponse,
+} from "../interfaces/pokeapi-response.interface";
 export class Pokemon {
     get imageUrl(): string {
         return `https://pokemon.com/${this.id}.jpg`;
@@ -16,10 +20,12 @@ export class Pokemon {
         console.log(`${this.name}, ${this.name}`);
     }
 
-    async getMoves() {
-        const { data } = await axios.get("https://pokeapi.co/api/v2/pokemon/4");
+    async getMoves(): Promise<Move[]> {
+        const { data } = await axios.get<PokeapiResponse>(
+            "https://pokeapi.co/api/v2/pokemon/4"
+        );
 
-        console.log("res", data.moves);
+        console.log(data);
         return data.moves;
     }
 }
